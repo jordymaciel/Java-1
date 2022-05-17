49bbce3ca81713038899c0d28958cc2dea4e250c
Jordy Maciel <jms59@discente.ifpe.edu.br>
2022-05-17 00:21:10 -0300

    Update numeros pares

---
+++ Lista1/Numerospares.java
@@ -0,0 +1,8 @@
+public class Numerospares{
+    public static void main (String[]args){
+        for (int n1 = 0; n1 <=30; n1 = n1 +2){
+            System.out.println(n1);
+        }
+            
+    }
+}
